c2c0518dce5b025c169856238a174133f2f28000
Rowan <rowan.taubitz@firmus.co>
2024-11-28 02:59:32 +0000

    Remove unused import

--- custom_components/brama_integration/__init__.py
+++ custom_components/brama_integration/__init__.py
@@ -14,7 +14,6 @@ from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.loader import async_get_loaded_integration
 
 from .api import BramaIntegrationApiClient
-from .const import DOMAIN
 from .coordinator import BlueprintDataUpdateCoordinator
 from .data import BramaIntegrationData
 
